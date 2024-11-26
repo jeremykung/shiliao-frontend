4542e817e613d0eb2f091e1f7d619b34c2b0293f
Jeremy Kung <jeremykung@gmail.com>
2024-11-26 19:50:41 +0200

    temporary rm add/edit for production

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -4,8 +4,8 @@
         <div>
             <NuxtLink class="nav-link" to="/">Home</NuxtLink>
             <NuxtLink class="nav-link" to="about">About</NuxtLink>
-            <NuxtLink class="nav-link" to="add">Add</NuxtLink>
-            <NuxtLink class="nav-link" to="edit">Edit</NuxtLink>
+            <!-- <NuxtLink class="nav-link" to="add">Add</NuxtLink> -->
+            <!-- <NuxtLink class="nav-link" to="edit">Edit</NuxtLink> -->
         </div>
     </div>
 </template>
