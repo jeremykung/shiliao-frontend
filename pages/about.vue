8c9468a8df382ac6fe4282b5d7685bf203c22bbf
Jeremy Kung <jeremykung@gmail.com>
2024-11-26 19:41:40 +0200

    added about blurb

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>About</h1>
+    <Navigation />
+    <div class="about-container">
+        <h1>About</h1>
+        <p>I made this project to make it easy to find which foods are hot and cold according to Chinese medicine and food therapy.</p>
+    </div>
 </template>
+
+<style scoped>
+.about-container {
+    width: 50%;
+    max-width: 500px;
+    min-width: 250px;
+    margin: 2rem auto;
+    text-align: center;
+}
+.about-container > h1 {
+    margin-bottom: 1rem;
+}
+</style>
