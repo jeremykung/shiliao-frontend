092ba6b7d02061b81db1d9331a6c9a0dc1c4a100
Jeremy Kung <jeremykung@gmail.com>
2024-12-12 21:48:58 +0200

    removed color bg

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,30 +3,40 @@
         <h1>食療 | Shi Liao</h1>
         <div>
             <NuxtLink class="nav-link" to="/">Home</NuxtLink>
-            <NuxtLink class="nav-link" to="about">About</NuxtLink>
             <!-- <NuxtLink class="nav-link" to="add">Add</NuxtLink> -->
             <!-- <NuxtLink class="nav-link" to="edit">Edit</NuxtLink> -->
+            <!-- <NuxtLink class="nav-link" to="login">Login</NuxtLink> -->
+            <NuxtLink class="nav-link" to="about">About</NuxtLink>
         </div>
+        <!-- <p>User: {{ userStore.email }}</p> -->
     </div>
 </template>
+
+<script lang="ts" setup>
+import { useUserStore } from '~/stores/user'
+
+const userStore = useUserStore()
+</script>
+
 
 <style scoped>
 .nav-container {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: var(--primary-color);
+    /* background-color: var(--primary-color); */
     padding: 20px;
 }
 .nav-container > h1 {
-    color: var(--primary-color-darker);
+    /* color: var(--primary-color-darker); */
 }
 .nav-link {
     padding: 16px;
     text-decoration: none;
-    background-color: var(--primary-color-dark);
+    /* background-color: var(--primary-color-dark); */
+    background-color: rgba(255, 255, 255, 0.7);
     font-weight: bold;
-    color: white;
+    /* color: white; */
     border-radius: 4px;
     margin-left: 10px;
 }
