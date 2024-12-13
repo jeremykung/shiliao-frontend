4fd5cb493e578881d5ca88cecb84ad38b072b855
Jeremy Kung <jeremykung@gmail.com>
2024-12-13 21:09:05 +0200

    rm user store

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -13,9 +13,9 @@
 </template>
 
 <script lang="ts" setup>
-import { useUserStore } from '~/stores/user'
+// import { useUserStore } from '~/stores/user'
 
-const userStore = useUserStore()
+// const userStore = useUserStore()
 </script>
 
 
