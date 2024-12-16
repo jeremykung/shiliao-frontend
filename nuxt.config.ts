0fe9eac777d3a8cd6fa044872c8608edfb984db9
Jeremy Kung <jeremykung@gmail.com>
2024-12-16 22:22:20 +0200

    updated config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   
   runtimeConfig: {
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_KEY: process.env.SUPABASE_KEY,
     public: {
-      TEST: "hi",
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_KEY: process.env.SUPABASE_KEY,
     }
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   devServer: { port: 8000 },
   css: ['~/assets/css/main.css'],
-  modules: [
-    '@nuxtjs/supabase', // defaults to "/login" page if enabled: https://www.reddit.com/r/Nuxt/comments/17a3p7r/nuxt_app_keeps_redirecting_to_login/
-    // 'nuxt-security',
-  ],
+  modules: [// defaults to "/login" page if enabled: https://www.reddit.com/r/Nuxt/comments/17a3p7r/nuxt_app_keeps_redirecting_to_login/
+  // 'nuxt-security',
+  '@nuxtjs/supabase', '@pinia/nuxt'],
   // security: {
   //   corsHandler: {
       
